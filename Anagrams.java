d9799e2eb9dd78bbf0f53aa02f27ec39b51d8b55
Uday Kumar Thangala <92805757+udaythangala@users.noreply.github.com>
2023-12-30 16:33:14 +0530

    Add files via upload

---
+++ Anagrams.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Anagrams{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String str1 = sc.next();
+		String str2 = sc.next();
+		sc.close();
+		char[] arr1 = str1.toLowerCase().toCharArray();
+		char[] arr2 = str2.toLowerCase().toCharArray();
+		if(str1.length() == str2.length())
+		{
+			Arrays.sort(arr1);
+			Arrays.sort(arr2);
+			if(Arrays.equals(arr1, arr2))
+			{
+				System.out.println("Anagrams");
+			}
+			else
+				System.out.println("NOt Anagrams");
+
+		}
+	}
+}		
